2931aba33e9e4b2bdc185913ac3613ad4972f51e
Turner-SCU <107722168+Turner-SCU@users.noreply.github.com>
2023-07-17 15:39:41 -0700

    documentation of data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Steps to produce the sample data provided in the MSD package
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `see data-raw file on GitHub to see process to generating timeseries and raster data`
+#' Daily precipitation data recorded from 1981 to 1985, trimmed to only highlight Nicaragua
+#'
+#' @source University of California, Santa Barbara Climate Hazards Group InfraRed Precipitation with Station data (CHIRPS)
+"timeseries"
