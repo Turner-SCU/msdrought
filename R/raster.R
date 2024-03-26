76583fbaebd94fbd2bedece1683d50deb8afc782
Turner-SCU <107722168+Turner-SCU@users.noreply.github.com>
2024-03-25 22:27:26 -0700

    removed empty vignettes/R files

--- R/raster.R
+++
@@ -1,9 +0,0 @@
-#' Steps to produce the raster sample data provided in the MSD package
-#'
-#' Daily precipitation data recorded from 1981 to 1985, trimmed to only highlight Nicaragua
-#'
-#' @format ## `see data-raw file on GitHub to see process to generating timeseries and raster data`
-#'
-#'
-#' @source University of California, Santa Barbara Climate Hazards Group InfraRed Precipitation with Station data (CHIRPS)
-"timeseries"
