9a1a16630fae7f4a75e68faf637160d03ff3c5f9
Turner-SCU <107722168+Turner-SCU@users.noreply.github.com>
2024-03-31 00:06:44 -0700

    replaced placeholder info

--- R/timeseries.R
+++ R/timeseries.R
@@ -1,7 +1,6 @@
-#' World Health Organization TB data
+#' MSD package included timeseries data
 #'
-#' A subset of data from the World Health Organization Global Tuberculosis
-#' Report ...
+#' A subset of data from the UCSB CHIRPS data set
 #'
 #' @format ## `timeseries`
 #' An xts object with precipitation data over a series of five years:
